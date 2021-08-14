d79016a541e0220908b7593bdd26592020743de8
guz86 <guzmo6@rambler.ru>
2021-08-14 03:20:11 +0300

    WriteError

--- Junior/Program.cs
+++ Junior/Program.cs
@@ -8,33 +8,26 @@ namespace Junior
         {
             Console.WriteLine("Привет");
 
+            WriteError();
+
+            Console.WriteLine("Не может быть");
+
+            WriteError();
+
+
+
+        }
+        static void WriteError()
+        {
             // запомним в переменную текущий цвет текста в консли
             ConsoleColor defaulColor = Console.ForegroundColor;
             // сообщение выводится красным
             Console.ForegroundColor = ConsoleColor.Red;
 
             Console.WriteLine("Ошибка. Нет интернета.");
-             
-            // возвращаем текущий цвет
-            Console.ForegroundColor = defaulColor;
-
-            Console.WriteLine("Не может быть");
-
-
-            // запомним в переменную текущий цвет текста в консли
-            defaulColor = Console.ForegroundColor;
-            // сообщение выводится красным
-            Console.ForegroundColor = ConsoleColor.Red;
-
-            Console.WriteLine("Ошибка. Нет интернета.");
 
             // возвращаем текущий цвет
             Console.ForegroundColor = defaulColor;
-
-        }
-        static void WriteError()
-        {
-
         }
     }
 }
