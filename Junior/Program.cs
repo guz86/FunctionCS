f130dad8b7f8e382907c208895a0eb7a1a42211e
guz86 <guzmo6@rambler.ru>
2021-08-14 03:24:49 +0300

    string text

--- Junior/Program.cs
+++ Junior/Program.cs
@@ -8,23 +8,24 @@ namespace Junior
         {
             Console.WriteLine("Привет");
 
-            WriteError();
+            WriteError("Ошибка. Нет интернета.");
 
             Console.WriteLine("Не может быть");
-
-            WriteError();
+            // в функцию из программы передается аргумент "Оплати интернет"
+            WriteError("Оплати интернет");
 
 
 
         }
-        static void WriteError()
+        // передаем параметр в функцию , что хотим получить string text
+        static void WriteError(string text)
         {
             // запомним в переменную текущий цвет текста в консли
             ConsoleColor defaulColor = Console.ForegroundColor;
             // сообщение выводится красным
             Console.ForegroundColor = ConsoleColor.Red;
 
-            Console.WriteLine("Ошибка. Нет интернета.");
+            Console.WriteLine(text);
 
             // возвращаем текущий цвет
             Console.ForegroundColor = defaulColor;
