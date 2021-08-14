ff7af2c7c7b3d742c7eba8bfbe7c8ba1640812d6
guz86 <guzmo6@rambler.ru>
2021-08-14 03:19:23 +0300

    init

--- Junior/Program.cs
+++ Junior/Program.cs
@@ -6,7 +6,35 @@ namespace Junior
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Привет");
+
+            // запомним в переменную текущий цвет текста в консли
+            ConsoleColor defaulColor = Console.ForegroundColor;
+            // сообщение выводится красным
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.WriteLine("Ошибка. Нет интернета.");
+             
+            // возвращаем текущий цвет
+            Console.ForegroundColor = defaulColor;
+
+            Console.WriteLine("Не может быть");
+
+
+            // запомним в переменную текущий цвет текста в консли
+            defaulColor = Console.ForegroundColor;
+            // сообщение выводится красным
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.WriteLine("Ошибка. Нет интернета.");
+
+            // возвращаем текущий цвет
+            Console.ForegroundColor = defaulColor;
+
+        }
+        static void WriteError()
+        {
+
         }
     }
 }
