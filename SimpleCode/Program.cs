12edf1c9b22ee53ca091f1353a3c88ef936f62f3
guz86 <guzmo6@rambler.ru>
2021-08-16 17:17:45 +0300

    PrintString

--- SimpleCode/Program.cs
+++ SimpleCode/Program.cs
@@ -4,9 +4,31 @@ namespace SimpleCode
 {
     class Program
     {
+
+        static void PrintString()
+        {
+            Console.Write("Введите символ: ");
+            string symbol = Console.ReadLine(); 
+            Console.Write("Введите количество: ");
+             int count = Convert.ToInt32(Console.ReadLine());
+            string result = "";
+            
+            for (int i = 1; i < count; i++)
+            {
+                result += symbol;
+            }
+            Console.WriteLine(result);
+        }
+
+
+
+
         static void Main(string[] args)
         {
-            
+            // ДЗ. Метод выводит на экран строку. Символы из которых состоит строка и их количество вводятся пользователем.
+            PrintString();
         }
+
+
     }
 }
