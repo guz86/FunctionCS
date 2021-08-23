406ba7684164b77c9a4d83b253581482b3e235af
guz86 <guzmo6@rambler.ru>
2021-08-23 15:56:55 +0300

    maps

--- PacMan/Program.cs
+++ PacMan/Program.cs
@@ -7,13 +7,25 @@ namespace PacMan
     {
         static void Main(string[] args)
         {
-            char[,] map;
+            char[,] map = ReadMap("map1");
         }
        // функция считывает готовую карту из файла
        static char [,] ReadMap(string mapName)
         {
             string[] newFile = File.ReadAllLines($"Maps/{mapName}.txt");
-            char[,] maps = new char[1,1];
+            // newFile.Length количество строчек
+            // newFile[0].Length - длина первой строчки
+            // содздаем карту и переносим в нее данные с файла
+            char[,] maps = new char[newFile.Length,newFile[0].Length];
+
+            for (int i = 0; i < maps.GetLength(0); i++)
+            {
+                for (int j = 0; j < maps.GetLength(1); j++)
+                {
+                    maps[i, j] = newFile[i][j];
+                }
+            }
+
 
             return maps;
 
