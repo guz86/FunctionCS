26834407d2cff46baf561fabc506fbe4f94d859f
guz86 <guzmo6@rambler.ru>
2021-08-24 17:33:07 +0300

    Key

--- PacMan/Program.cs
+++ PacMan/Program.cs
@@ -7,17 +7,64 @@ namespace PacMan
     {
         static void Main(string[] args)
         {
-            int packmanX, packmanY;
-            char[,] map = ReadMap("map1", out packmanX, out packmanY);
+            Console.CursorVisible = false;
+
+
+            bool isPlaying = true;
+
+            int pacmanX, pacmanY;
+            int pacmanDX = 0, pacmanDY = 1;
+
+            char[,] map = ReadMap("map1", out pacmanX, out pacmanY);
 
             DrawMap(map);
 
-            Console.SetCursorPosition(packmanY, packmanX);
-            Console.Write('@');
+            while (isPlaying)
+            {
+                Console.SetCursorPosition(pacmanY, pacmanX);
+                Console.Write('@');
+
+                // без символов
+                //Console.ReadKey(true);
+
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey(true);
+                    switch (key.Key)
+                    {
+                        
+                        case ConsoleKey.LeftArrow:
+                            pacmanDX = 0; pacmanDY = -1;
+                            break;
+                        case ConsoleKey.UpArrow:
+                            pacmanDX = -1; pacmanDY = 0;
+                            break;
+                        case ConsoleKey.RightArrow:
+                            pacmanDX = 0; pacmanDY = 1;
+                            break;
+                        case ConsoleKey.DownArrow:
+                            pacmanDX = 1; pacmanDY = 0;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                if (map[pacmanX+ pacmanDX, pacmanY + pacmanDY] != '#')
+                {
+                    Console.SetCursorPosition(pacmanY, pacmanX);
+                    Console.Write(" ");
+                    pacmanY += pacmanDY;
+                    pacmanX += pacmanDX;
+                    Console.SetCursorPosition(pacmanY, pacmanX);
+                    Console.Write("@");
+                }
+                System.Threading.Thread.Sleep(200);
+            }
+
 
         }
-       // функция считывает готовую карту из файла
-       static char [,] ReadMap(string mapName, out int packmanX, out int packmanY)
+        // функция считывает готовую карту из файла
+        static char[,] ReadMap(string mapName, out int packmanX, out int packmanY)
         {
             //инициализация для работы
             packmanX = 0;
@@ -26,14 +73,14 @@ namespace PacMan
             // newFile.Length количество строчек
             // newFile[0].Length - длина первой строчки
             // содздаем карту и переносим в нее данные с файла
-            char[,] maps = new char[newFile.Length,newFile[0].Length];
+            char[,] maps = new char[newFile.Length, newFile[0].Length];
 
             for (int i = 0; i < maps.GetLength(0); i++)
             {
                 for (int j = 0; j < maps.GetLength(1); j++)
                 {
                     maps[i, j] = newFile[i][j];
-                    if (maps[i,j] == '@')
+                    if (maps[i, j] == '@')
                     {
                         packmanX = i;
                         packmanY = j;
@@ -42,7 +89,7 @@ namespace PacMan
             }
             return maps;
         }
-        
+
         // отрисовываем карту
         static void DrawMap(char[,] map)
         {
@@ -50,7 +97,7 @@ namespace PacMan
             {
                 for (int j = 0; j < map.GetLength(1); j++)
                 {
-                    Console.Write(map[i,j]);
+                    Console.Write(map[i, j]);
                 }
                 Console.WriteLine();
             }
