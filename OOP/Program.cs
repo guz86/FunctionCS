1c2783d54409cba327a4d940813d20a25160dd8b
Pavel Gordienko <guzmo6@rambler.ru>
2021-12-14 05:41:34 +0300

    Tank

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -6,7 +6,21 @@ namespace OOP
     {
         static void Main(string[] args)
         {
-            
+            Tank tankOne = new Tank();
+            tankOne.Armor = 100;
+            Tank tankTwo;
+            tankTwo = tankOne;
+            tankTwo.Speed = 900;
+            Console.WriteLine($"Armor Tank Two = {tankTwo.Armor}");
+            Console.WriteLine($"Armor Tank One = {tankTwo.Speed}");
         }
+    }
+    
+    class Tank
+    {
+        public int Armor;
+        int _damage;
+        public int Speed;
+
     }
 }
