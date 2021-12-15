c8e352e2fa5d1b4ab6216f146bac4e8eedd7882e
Pavel Gordienko <guzmo6@rambler.ru>
2021-12-15 06:33:37 +0300

    Table

--- cafe/Program.cs
+++ cafe/Program.cs
@@ -6,7 +6,15 @@ namespace cafe
     {
         static void Main(string[] args)
         {
-            
+            Table tableOne = new Table();
+            tableOne.MaxPlace = 10;
         }
+    }
+
+    class Table
+    {
+        public int NumberTable;
+        public int FreePlace;
+        public int MaxPlace;
     }
 }
