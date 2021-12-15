e878fbb4b50c2572c9138e274f5916876952bb5f
Pavel Gordienko <guzmo6@rambler.ru>
2021-12-15 06:41:17 +0300

    output

--- cafe/Program.cs
+++ cafe/Program.cs
@@ -6,8 +6,23 @@ namespace cafe
     {
         static void Main(string[] args)
         {
-            Table tableOne = new Table();
-            tableOne.MaxPlace = 10;
+            Table tableOne = new Table(1,10);
+            // tableOne.MaxPlace = 10;
+            Table tableTwo = new Table(2, 20);
+
+
+            // вывод
+            bool isOpen = true;
+
+            while (isOpen)
+            {
+                Console.WriteLine($"{tableOne.NumberTable}, {tableOne.MaxPlace}, {tableOne.FreePlace}");
+                Console.WriteLine($"{tableTwo.NumberTable}, {tableTwo.MaxPlace}, {tableTwo.FreePlace}");
+
+                Console.ReadKey();
+            }
+
+
         }
     }
 
@@ -16,5 +31,17 @@ namespace cafe
         public int NumberTable;
         public int FreePlace;
         public int MaxPlace;
+
+        // конструктор для заполнения
+        public Table(int number, int maxplace)
+        {
+            NumberTable = number;
+            MaxPlace = maxplace;
+            //все места свободные при создании стола
+            FreePlace = maxplace;
+        }
+
     }
+
+
 }
