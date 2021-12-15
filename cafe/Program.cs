cca7aed39734a47cb0a4ce769b1c02836efdc12a
Pavel Gordienko <guzmo6@rambler.ru>
2021-12-15 06:46:48 +0300

    array table and output

--- cafe/Program.cs
+++ cafe/Program.cs
@@ -6,18 +6,26 @@ namespace cafe
     {
         static void Main(string[] args)
         {
-            Table tableOne = new Table(1,10);
+            //Table tableOne = new Table(1,10);
             // tableOne.MaxPlace = 10;
-            Table tableTwo = new Table(2, 20);
+            //Table tableTwo = new Table(2, 20);
 
+            // массив столов
+            Table[] tables = {new Table(1, 10) , new Table(2, 20), new Table(3, 25) };
 
             // вывод
             bool isOpen = true;
 
             while (isOpen)
             {
-                Console.WriteLine($"{tableOne.NumberTable}, {tableOne.MaxPlace}, {tableOne.FreePlace}");
-                Console.WriteLine($"{tableTwo.NumberTable}, {tableTwo.MaxPlace}, {tableTwo.FreePlace}");
+                //Console.WriteLine($"{tableOne.NumberTable}, {tableOne.MaxPlace}, {tableOne.FreePlace}");
+                //Console.WriteLine($"{tableTwo.NumberTable}, {tableTwo.MaxPlace}, {tableTwo.FreePlace}");
+
+                for (int i = 0; i < tables.Length; i++)
+                {
+                    tables[i].InfoTable();
+                }
+
 
                 Console.ReadKey();
             }
@@ -39,6 +47,12 @@ namespace cafe
             MaxPlace = maxplace;
             //все места свободные при создании стола
             FreePlace = maxplace;
+        }
+
+        // вывод информации о столе
+        public void InfoTable()
+        {
+            Console.WriteLine($"{NumberTable}, {MaxPlace}, {FreePlace}");
         }
 
     }
