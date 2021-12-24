5246bd5186eb0c301e7bb0b081eac88dd633d370
Pavel Gordienko <guzmo6@rambler.ru>
2021-12-24 08:10:32 +0300

    foreach

--- virtual_method/Program.cs
+++ virtual_method/Program.cs
@@ -6,13 +6,20 @@ namespace virtual_method
     {
         static void Main(string[] args)
         {
-            
+            //int[] array  = new int[4];
+              NPS[] players = new NPS[] {new NPS(), new Farmer(), new Warrior(), new Child()};
+
+            foreach (var player in players)
+            {
+                player.ShowInfo();
+                Console.WriteLine(" ");
+            }
         }
     }
 
     class NPS
     {
-        public void ShowInfo()
+        public virtual void ShowInfo()
         {
             Console.WriteLine("Я ничего не делаю.");
         }
@@ -20,11 +27,19 @@ namespace virtual_method
 
     class Farmer : NPS
     {
-
+        public override void ShowInfo()
+        {
+            base.ShowInfo();
+            Console.WriteLine("Пойду поработаю.");
+        }
     }
 
     class Warrior : NPS
     {
+        public override void ShowInfo()
+        {
+            Console.WriteLine("Пойду потренируюсь, скоро в бой");
+        }
 
     }
 
