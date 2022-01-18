2e9c66a0a626f219d70eef9f7094acadf1495686
Pavel Gordienko <guzmo6@rambler.ru>
2022-01-19 02:06:12 +0300

    vehicles

--- AbstractClasses/Program.cs
+++ AbstractClasses/Program.cs
@@ -6,14 +6,22 @@ namespace AbstractClasses
     {
         static void Main(string[] args)
         {
-            Vehicle сar1 = new Car();
-            сar1.Move();
+            // сar1 = new Car();
+            //сar1.Move();
 
-            Car car2 = new Car();
-            car2.Move();
+            //Car car2 = new Car();
+            //car2.Move();
 
-            Vehicle tractor1 = new Tractor();
-            tractor1.Move();
+            //Vehicle tractor1 = new Tractor();
+            //tractor1.Move();
+
+            Vehicle[] vehicles = {new Car(), new Tractor()};
+
+            foreach (var vehicle in vehicles)
+            {
+                vehicle.Move();
+                Console.WriteLine(vehicle.GetCurrentSpeed());
+            }
         }
     }
 }
