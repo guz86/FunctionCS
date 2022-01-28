1616d370d4926ced98bc98e54af0c2c8fddf69fa
Pavel Gordienko <guzmo6@rambler.ru>
2022-01-28 05:16:36 +0300

    Computer

--- OOPPractice/Computer.cs
+++ OOPPractice/Computer.cs
@@ -9,23 +9,61 @@ namespace OOPPractice
     class Computer
     {
         // за ПК может сесть школьник (кто то) (место, слот)
+        private SchoolBoy _schoolboy;
         // количество минут за компьютером, сколько их осталось
+        private int _minutesLeft;
         // у каждого пк есть своя цена за минуту времени
+        public int PriceForMinutes { get; private set; }
         // компьютер занят в данный момент или свободен?
-        
-        //действия
-        // за компьютер можно сесть.
-        // компьютер можно освободить.
-
+        public bool isBusy
+        {
+            get
+            {
+                return _minutesLeft > 0;
+            }
+        }
         // каждый школьник занимает пк на определенное количество
         // минут
 
         // конструктор?
+        public Computer(int priceForMinutes)
+        {
+            PriceForMinutes = priceForMinutes;
+        }
         
+        //действия
+        // за компьютер можно сесть.
+        public void TakeThePlace(SchoolBoy schoolBoy)
+        {
+            _schoolboy = schoolBoy;
+            _minutesLeft = _schoolboy.DesiredMinutes;
+
+        }
+        // компьютер можно освободить.
+        public void FreeThePlace()
+        {
+            _schoolboy = null;
+        }
 
         // отчет времени за пк по минутам..
 
+        public void SkipMinutes()
+        {
+            _minutesLeft--;
+        }
+
         // отображение информации о пк, занят,
+        public void ShowInfo()
+        {
+            if (isBusy)
+            {
+                Console.WriteLine($"Пк занят, осталось {_minutesLeft}");
+            }
+            else
+            {
+                Console.WriteLine($"Пк свободен, цена за минуту {PriceForMinutes }");
+            }
+        }
         // свободен сколько времени осталось
 
     }
