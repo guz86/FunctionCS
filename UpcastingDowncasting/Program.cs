792963564b612cc7252e9b5f4147c88ee71a478c
Pavel Gordienko <guzmo6@rambler.ru>
2022-02-06 11:56:56 +0300

    test rider

--- UpcastingDowncasting/Program.cs
+++ UpcastingDowncasting/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace UpcastingDowncasting
 {
@@ -6,7 +7,7 @@ namespace UpcastingDowncasting
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("test");
         }
     }
 }
