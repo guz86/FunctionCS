d1c546a579a925cfeda36ce2be57d77629826e8e
Pavel Gordienko <guzmo6@rambler.ru>
2022-02-07 15:36:55 +0300

    as is

--- as_is/Program.cs
+++ as_is/Program.cs
@@ -7,6 +7,35 @@ namespace as_is
         static void Main(string[] args)
         {
             Person person = new Person("Ivan");
+            Person person1 = new Mentor("Alexey",10);
+
+            //System.InvalidCastException: 'Unable to cast object of type
+            //'as_is.Person' to type 'as_is.Mentor'.'
+            //Mentor mentor = (Mentor)person;
+
+            Mentor mentor = (Mentor)person1;
+
+            Mentor mentor1 = person as Mentor;
+            // mentor1 null
+
+            Mentor mentor2 = person1 as Mentor;
+            mentor2.ShowInfo();
+
+            // простая проверка перед преобразованием
+
+            if (person1 is Mentor)
+            {
+                Mentor mentor3 = (Mentor)person1;
+                mentor3.ShowInfo();
+            }
+
+            // если да то передаем в mentor4
+
+            if (person1 is Mentor mentor4)
+            {
+                mentor4.ShowInfo();
+            }
+
         }
     }
 }
