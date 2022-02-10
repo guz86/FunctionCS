c8ac7aac3bb165a81f47bf600a39217c3640b1f3
Pavel Gordienko <guzmo6@rambler.ru>
2022-02-11 01:40:52 +0300

    init

---
+++ Enum/Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum
+{
+    class Game
+    {
+        private string _title;
+        private string _genre;
+
+        public Game(string title, string ganre)
+        {
+            _title = title;
+            _genre = ganre;
+        }
+
+    }
+}
---
+++ Enum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //электронный учет игр
+            List<Game> gameCollection = new List<Game>();
+
+            // вручную
+            gameCollection.Add(new Game("Black", "Strategy"));
+            gameCollection.Add(new Game("Witcher", "RPG"));
+            gameCollection.Add(new Game("Civilization", "Strategy"));
+            gameCollection.Add(new Game("Wisp's", "Action"));
+
+            // чтобы каждый раз не писать повторяющиеся жанры
+
+        }
+    }
+}
