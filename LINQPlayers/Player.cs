a97c857a9c06344c88be5c9be2289aefbe8693b8
Pavel Gordienko <guzmo6@rambler.ru>
2022-02-15 01:49:33 +0300

    init

---
+++ LINQPlayers/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQPlayers
+{
+    class Player
+    {
+        public string Login { get; private set; }
+
+        public int Level { get; private set; }
+        public Player(string login, int level)
+        {
+            Login = login;
+            Level = level;
+        }
+    }
+}
---
+++ LINQPlayers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LINQPlayers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // выборка игроков из массива у кого уровень силы больше 200
+
+            List<Player> players = new List<Player>();
+
+        }
+    }
+}
