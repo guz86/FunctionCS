88e6096bbe1b5034ba575867411779d550878097
Pavel Gordienko <guzmo6@rambler.ru>
2022-03-13 00:51:44 +0300

    test2

---
+++ Junior/EndCourse/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EndCourse
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Test");
+            Console.WriteLine("Test");
+        }
+    }
+}
