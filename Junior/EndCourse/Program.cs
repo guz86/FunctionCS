aca9cc1641c2beab9c5790e72f430379d4c5bf6c
Pavel Gordienko <guzmo6@rambler.ru>
2022-03-13 21:01:38 +0300

    Value Types and Reference Types

--- Junior/EndCourse/Program.cs
+++ Junior/EndCourse/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace EndCourse
 {
@@ -6,8 +7,25 @@ namespace EndCourse
     {
         public static void Main(string[] args)
         {
+            // Value Types and Reference Types
+            
+            //Value Types (в куче) в стеке  - при передаче передается копия значения переменной
+            //    -числовые, bool, пользовательские структуры
+
+            //Reference Types - в куче - при передаче передается ссылка на значение
+            //    -классы, делегаты, интерфейсы, dynamic, object, String, Массивы
+            
+            //1
+            int a = 2;
+            int b = 4;
+            a = b;
+            Console.WriteLine($"{a}, {b}"); // 4 4  происходит копирование значения
             
             
+            
+            
+
+
             // 6
             string message = "";
             for (int i = 0; i < 10; i++)
@@ -16,6 +34,7 @@ namespace EndCourse
             }
 
             Console.WriteLine(message);
+    
         }
     }
 }
