8417403e27a55a664faa4d852a7d0b21bf4695e7
Pavel Gordienko <guzmo6@rambler.ru>
2022-03-16 21:04:56 +0300

    row Image

--- Junior/Tasks/Program.cs
+++ Junior/Tasks/Program.cs
@@ -1,3 +1,39 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 //Console.WriteLine("Hello, World!");
+
+//namespace Tasks
+//{
+ //  class Program
+//   {
+//      static void Main(string[] args){
+      
+         int a = 10;
+         int b = 38 * 17; //646
+         int c = (31 - 5 * a) / b; //  выражение = 0.02
+         Console.WriteLine(c); // 0 
+
+
+         // Картинки
+         // На экране, в специальной зоне, выводятся картинки, по 3 в ряд. Всего у пользователя в альбоме 52 картинки.
+         // Код должен вывести, сколько полностью заполненных рядов можно будет вывести, и сколько картинок будет
+         //     сверх меры.
+         // В качестве решения ожидаются объявленные переменные с необходимыми значениями и, основываясь 
+         // на значениях переменных, вывод необходимых данных.
+         int maxImage = 52;
+         int countImage = 3;
+         int row = maxImage / 3;
+         int lagImage = maxImage % 3;
+         Console.WriteLine($"raw: {row}");
+         Console.WriteLine($"lagImage: {lagImage}");
+        
+        
+
+
+
+
+//     } 
+//   }
+
+//}
+
