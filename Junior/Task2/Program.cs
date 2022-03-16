fe693a1a4b83ed11a3ed431e99b469bed231a04a
Pavel Gordienko <guzmo6@rambler.ru>
2022-03-17 00:58:15 +0300

    Message

---
+++ Junior/Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Условные операторы и циклы
+// Освоение циклов
+// При помощи циклов вы можете повторять один и тот же код множество раз.
+//     Напишите простейшую программу, которая выводит указанное(установленное)
+// пользователем сообщение заданное количество раз.
+
+Console.WriteLine("Введите сообщение");
+string Message = Console.ReadLine();
+Console.WriteLine("Введите сколько раз его нужно вывести");
+int.TryParse(Console.ReadLine(),out int count);
+while (count > 0)
+{
+    Console.WriteLine(Message);
+    count--;
+}
