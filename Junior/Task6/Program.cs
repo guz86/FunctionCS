5f4178981cdceaee9701640558e2dd790a1b76e3
Pavel Gordienko <guzmo6@rambler.ru>
2022-03-18 02:22:43 +0300

    ver1

---
+++ Junior/Task6/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Вывод имени
+            // Вывести имя в прямоугольник из символа, который введет сам пользователь.
+            // Вы запрашиваете имя, после запрашиваете символ, а после отрисовываете в консоль его имя в прямоугольнике из его символов.
+            // Пример:
+            // Alexey
+            // %
+            //
+            // %%%%%%
+            // % Alexey %
+            // %%%%%%
+            //
+            // Примечание:
+            // Длину строки можно всегда узнать через свойство Length
+            // string someString = “Hello”;
+            // Console.WriteLine(someString.Length); //5
+
+            string name;
+            char symbol;
+
+            Console.Write("Введите имя:");
+            name = Console.ReadLine();
+            Console.WriteLine("Введите символ:  ");
+            symbol = Convert.ToChar(Console.ReadLine());
+
+
+            for (int i = 0; i < name.Length + 4; i++)
+            {
+                Console.Write(symbol);
+            }
+
+            Console.Write("\n" + symbol + " " + name + " " + symbol + "\n");
+
+            for (int i = 0; i < name.Length + 4; i++)
+            {
+                Console.Write(symbol);
+            }
+
+            Console.Write("\n");
+        }
+    }
+}
