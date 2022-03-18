1c43025e46e05d694f0d5fc2dbb8a21b5d5eb89b
Pavel Gordienko <guzmo6@rambler.ru>
2022-03-18 18:55:36 +0300

    ver1

--- Junior/Task10/Program.cs
+++ Junior/Task10/Program.cs
@@ -9,10 +9,48 @@
             // Найти наибольший элемент матрицы A(10,10) и записать ноль в ту ячейку,
             // где он находится. Вывести наибольший элемент, исходную и полученную матрицу.
             //     Массив под измененную версию не нужен.
-            
-            
 
+            int[,] myArray =
+            {
+                {3, 33, 425, 223, 543, 223, 123, 123, 421, 1234}, {3, 33, 425, 223, 543, 223, 123, 123, 421, 1234},
+                {3, 33, 425, 223, 543, 223, 123, 123, 421, 1234}, {3, 33, 425, 223, 543, 223, 123, 123, 421, 1234},
+                {3, 33, 425, 223, 543, 223, 123, 123, 421, 1234}, {3, 33, 425, 223, 543, 223, 123, 123, 421, 1234},
+                {3, 33, 425, 223, 543, 223, 123, 123, 421, 1234}, {3, 33, 425, 223, 543, 223, 123, 123, 421, 1234},
+                {3, 33, 425, 223, 543, 25523, 123, 123, 421, 1234},
+                {3, 33, 425, 223, 543, 223, 123, 123, 421, 1234},
+            };
+            int maxElement = int.MinValue;
+            int i = 0, jdMaxElement = 0, kdMaxElement = 0;
+
+            for (int j = 0; j < myArray.GetLength(0); j++)
+            {
+                for (int k = 0; k < myArray.GetLength(1); k++)
+                {
+                    if (maxElement < myArray[j, k])
+                    {
+                        maxElement = myArray[j, k];
+                        jdMaxElement = j;
+                        kdMaxElement = k;
+                    }
+
+                    Console.Write(myArray[j, k] + " ");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Max Element " + myArray[jdMaxElement, kdMaxElement]);
+            myArray[jdMaxElement, kdMaxElement] = 0;
+
+            for (int j = 0; j < myArray.GetLength(0); j++)
+            {
+                for (int k = 0; k < myArray.GetLength(1); k++)
+                {
+                    Console.Write(myArray[j, k] + " ");
+                }
+
+                Console.WriteLine();
+            }
         }
-
     }
 }
