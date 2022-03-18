3cbaedb218a650df685dc0be2dda37dbf55cfe2e
Pavel Gordienko <guzmo6@rambler.ru>
2022-03-18 19:39:43 +0300

    local maximum ver1

--- Junior/Task11/Program.cs
+++ Junior/Task11/Program.cs
@@ -6,11 +6,40 @@
         {
             // Локальные максимумы
             // Дан одномерный массив целых чисел из 30 элементов.
-            //     Найдите все локальные максимумы и вывести их. (Элемент является локальным максимумом,
+            // Найдите все локальные максимумы и вывести их. (Элемент является локальным максимумом,
             // если он не имеет соседей, больших, чем он сам)
             // Крайние элементы являются локальными максимумами если не имеют соседа большего, чем они сами.
-            //     Программа должна работать с массивом любого размера.
-            //     Массив всех локальных максимумов не нужен.
+            // Программа должна работать с массивом любого размера.
+            // Массив всех локальных максимумов не нужен.
+
+            // generate myArray
+            Random rand = new Random();
+            int[] myArray = new int[30];
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                myArray[i] = rand.Next(1, 999);
+                Console.Write(myArray[i]+ " ");
+            }
+
+            Console.WriteLine();
+            // local maximum ver1
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                if (i == 0)
+                {
+                    if(myArray[i] > myArray[i+1]) Console.Write(myArray[i] + " ");
+                    
+                }
+                else if (i == myArray.Length - 1) 
+                {
+                    if(myArray[i] > myArray[i-1]) Console.Write(myArray[i] + " ");
+                }
+                else 
+                {
+                    if (myArray[i] > myArray[i-1] && myArray[i] > myArray[i+1]) Console.Write(myArray[i] + " ");
+                }
+            }
+
         }
 
     }
