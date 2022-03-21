a169b64b04f51fbf867898fde8482b5b72c03dae
Pavel Gordienko <guzmo6@rambler.ru>
2022-03-21 05:21:26 +0300

    ver2

--- Junior/Task15/Program.cs
+++ Junior/Task15/Program.cs
@@ -8,7 +8,8 @@
             // Разработайте функцию, которая рисует некий бар (Healthbar, Manabar) в определённой позиции. Она также принимает некий закрашенный процент.
             //     При 40% бар выглядит так:
             // [####______]
-            Draw(40, ConsoleColor.Green);
+            //Draw(40, ConsoleColor.Green);
+            Draw2(30, ConsoleColor.Red);
         }
         //ver1
         static void Draw(int position, ConsoleColor color)
@@ -27,6 +28,25 @@
             }
             Console.Write("]");
         }
-
+        //ver2
+        static void Draw2(int position, ConsoleColor color)
+        {
+            ConsoleColor defaultColor = Console.BackgroundColor;
+            Console.Write("[");
+            string bar = "";
+            Console.BackgroundColor = color;
+            for (int i = 0; i < position / 10; i++)
+            {
+                bar += "#";
+            }
+            Console.Write(bar);
+            Console.BackgroundColor = defaultColor;
+            bar = "";
+            for (int i = position / 10; i < (100 / 10); i++)
+            {
+                bar += "_";
+            }
+            Console.Write(bar + "]");
+        }
     }
 }
