6b7113455a0778753e027b80cf9dac6a5f60ac03
Pavel Gordienko <guzmo6@rambler.ru>
2022-03-22 23:14:08 +0300

    refactoring

--- Junior/Task18/Program.cs
+++ Junior/Task18/Program.cs
@@ -10,35 +10,28 @@
             // Если такого слова нет, то следует вывести соответствующее сообщение.
 
             // ver2
-            string input;
+            string input = "";
+            
+            Dictionary<string, string> myDictionary = new Dictionary<string, string>();
+            myDictionary.Add("бабенка", "Молодая бойкая женщина");
+            myDictionary.Add("багаж", "Вещи, груз пассажиров, упакованные для отправки, перевозки");
+            myDictionary.Add("багор", "Шест с металлическим крюком и остриём");
+            myDictionary.Add("базис", "В материалистическом социологическом учении: совокупность");
+            
             while (true)
             {
-                Dictionary<string, string> myDictionary = new Dictionary<string, string>();
-                myDictionary.Add("бабенка", "Молодая бойкая женщина");
-                myDictionary.Add("багаж", "Вещи, груз пассажиров, упакованные для отправки, перевозки");
-                myDictionary.Add("багор", "Шест с металлическим крюком и остриём");
-                myDictionary.Add("базис", "В материалистическом социологическом учении: совокупность");
-
                 Console.Write("Введите слово(бабенка, багаж, багор, базис): ");
                 input = Console.ReadLine();
 
                 if (input == "exit") break;
                 
-                bool keyCheck = false;
-
-                if (input != null) keyCheck = myDictionary.ContainsKey(input);
-                
-                if (keyCheck)
+                if (input != null && myDictionary.ContainsKey(input))
                 {
-                    Console.WriteLine($"\"{input}\" - {myDictionary[input]}");
-                }
-                else if (input == "")
-                {
-                    Console.WriteLine("Вы ничего не ввели");
+                    Console.WriteLine($"{myDictionary[input]}");
                 }
                 else
                 {
-                    Console.WriteLine($"\"{input}\" -  нет в словаре");
+                    Console.WriteLine($"Нет в словаре");
                 }
 
                 Console.ReadKey();
