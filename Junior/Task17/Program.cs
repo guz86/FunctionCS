20e8671015b27a14458fa35681bb1f45b535bca2
Pavel Gordienko <guzmo6@rambler.ru>
2022-03-22 03:32:36 +0300

    ver1

--- Junior/Task17/Program.cs
+++ Junior/Task17/Program.cs
@@ -4,14 +4,34 @@
     {
         static void Main(string[] args)
         {
-            // BRAVE NEW WORLD
-            //     Сделать игровую карту с помощью двумерного массива. Сделать функцию рисования карты.
-            // Помимо этого, дать пользователю возможность перемещаться по карте и взаимодействовать
-            // с элементами (например пользователь не может пройти сквозь стену)
-            // Все элементы являются обычными символами
-            
+            // КАНЗАС СИТИ ШАФТ
+            //Реализуйте функцию Shuffle, которая перемешивает элементы массива в случайном порядке.
 
+            int[] myArray = {1,2,3,4,5,6,7,8,9,0};
+            Shuffle(myArray);
+            ShowArray(myArray);
+        }
+        
+        // ver1
+        static void Shuffle(int[] array)
+        {
+            Random rand = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                int position = rand.Next(1, 10);
+                int tempValue = array[position];
+                 array[position] = array[i];
+                 array[i] = tempValue;
+            }
         }
 
+        static void ShowArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]);
+            }
+        }
     }
 }
+
