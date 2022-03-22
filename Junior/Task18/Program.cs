4a939090096fcb64eb4a0643da9a5d2ddcaa3aa2
Pavel Gordienko <guzmo6@rambler.ru>
2022-03-22 20:26:28 +0300

    ver1

--- Junior/Task18/Program.cs
+++ Junior/Task18/Program.cs
@@ -8,7 +8,41 @@
             //     Толковый словарь
             // Создать программу, которая принимает от пользователя слово и выводит его значение.
             // Если такого слова нет, то следует вывести соответствующее сообщение.
+            
+            // ver1
+            bool exit = true;
+            string input;
+            while (exit)
+            {
+                Dictionary<string, string> myDictionary = new Dictionary<string,string>();
+                myDictionary.Add("бабенка","Молодая бойкая женщина");
+                myDictionary.Add("багаж","Вещи, груз пассажиров, упакованные для отправки, перевозки");
+                myDictionary.Add("багор","Шест с металлическим крюком и остриём");
+                myDictionary.Add("базис","В материалистическом социологическом учении: совокупность");
 
+                Console.WriteLine("Введите слово(бабенка, багаж, багор, базис):");
+                input = Console.ReadLine();
+                bool keyCheck = false;
+                
+                if (!String.IsNullOrEmpty(input))
+                {
+                    keyCheck = myDictionary.ContainsKey(input);
+                }
+                
+                if (keyCheck)
+                {
+                    Console.WriteLine(myDictionary[input]);
+                }
+                
+                if (input == "exit")
+                {
+                    exit = false;
+                }
+
+                Console.ReadKey();
+                Console.Clear();
+
+            }
         }
 
     }
