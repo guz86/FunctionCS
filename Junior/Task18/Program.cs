a197b36525023f15fcfb4518fddddcf98962be41
Pavel Gordienko <guzmo6@rambler.ru>
2022-03-22 20:36:27 +0300

    ver2

--- Junior/Task18/Program.cs
+++ Junior/Task18/Program.cs
@@ -8,42 +8,43 @@
             //     Толковый словарь
             // Создать программу, которая принимает от пользователя слово и выводит его значение.
             // Если такого слова нет, то следует вывести соответствующее сообщение.
-            
-            // ver1
-            bool exit = true;
-            string input;
-            while (exit)
-            {
-                Dictionary<string, string> myDictionary = new Dictionary<string,string>();
-                myDictionary.Add("бабенка","Молодая бойкая женщина");
-                myDictionary.Add("багаж","Вещи, груз пассажиров, упакованные для отправки, перевозки");
-                myDictionary.Add("багор","Шест с металлическим крюком и остриём");
-                myDictionary.Add("базис","В материалистическом социологическом учении: совокупность");
 
-                Console.WriteLine("Введите слово(бабенка, багаж, багор, базис):");
+            // ver2
+            string input;
+            while (true)
+            {
+                Dictionary<string, string> myDictionary = new Dictionary<string, string>();
+                myDictionary.Add("бабенка", "Молодая бойкая женщина");
+                myDictionary.Add("багаж", "Вещи, груз пассажиров, упакованные для отправки, перевозки");
+                myDictionary.Add("багор", "Шест с металлическим крюком и остриём");
+                myDictionary.Add("базис", "В материалистическом социологическом учении: совокупность");
+
+                Console.Write("Введите слово(бабенка, багаж, багор, базис): ");
                 input = Console.ReadLine();
                 bool keyCheck = false;
-                
+
+                if (input == "exit")
+                {
+                    break;
+                }
+
                 if (!String.IsNullOrEmpty(input))
                 {
                     keyCheck = myDictionary.ContainsKey(input);
                 }
-                
+
                 if (keyCheck)
                 {
-                    Console.WriteLine(myDictionary[input]);
+                    Console.WriteLine($"\"{input}\" - {myDictionary[input]}");
                 }
-                
-                if (input == "exit")
+                else
                 {
-                    exit = false;
+                    Console.WriteLine($"\"{input}\" -  нет в словаре");
                 }
 
                 Console.ReadKey();
                 Console.Clear();
-
             }
         }
-
     }
 }
