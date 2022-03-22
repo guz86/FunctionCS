d8963696740efd38e1de507c74086631fd123b0e
Pavel Gordienko <guzmo6@rambler.ru>
2022-03-23 00:12:24 +0300

    ver1

--- Junior/Task19/Program.cs
+++ Junior/Task19/Program.cs
@@ -11,8 +11,36 @@
             //     После обслуживания каждого клиента программа ожидает нажатия любой клавиши, после чего затирает
             // консоль и по новой выводит всю информацию, только уже со следующим клиентом
 
-            
+            //ver1
+            var myQueue = AddQueue(15);
+            int money = 0;
+            int i = 1;
+
+            while (myQueue.Count > 0)
+            {
+                money += myQueue.Dequeue();
+                Console.WriteLine($"Обслужили клиента номер {i}, " + $"осталось в очереди {myQueue.Count} человек" +
+                                  $"\n На счету оказалось {money}");
+                Console.ReadLine();
+                Console.Clear();
+                i++;
+            }
+
+            Console.WriteLine($"Всего заработано: {money}");
         }
 
+        static Queue<int> AddQueue(int count)
+        {
+            Random rand = new Random();
+            Queue<int> queue = new Queue<int>();
+
+            while (queue.Count < count)
+            {
+                int value = rand.Next(100, 1000);
+                queue.Enqueue(value);
+            }
+
+            return queue;
+        }
     }
 }
