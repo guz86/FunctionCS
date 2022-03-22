9d55c705f9286289be1fb66458fd2196a3d48b53
Pavel Gordienko <guzmo6@rambler.ru>
2022-03-23 01:00:09 +0300

    //ver1

--- Junior/Task21/Program.cs
+++ Junior/Task21/Program.cs
@@ -14,7 +14,85 @@
             // 3) удалить досье
             // 4) выход
 
+            //ver1
+            Dictionary<string, string> Person = new Dictionary<string, string>();
+            //string[] FIO = new string[0];
+            //string[] job = new string[0];
+            bool exit = false;
+            while (!exit)
+            {
+                Console.WriteLine("(1) добавить досье (2) вывести все досье (3) удалить досье (4) выход");
+                bool checkEnter = int.TryParse(Console.ReadLine(), out int enter);
+                if (!checkEnter)
+                {
+                    Console.WriteLine("Вы ввели, что то не то, нажмите любую клавишу");
+                }
+
+                switch (@enter)
+                {
+                    case 1:
+                        AddFile(Person);
+                        break;
+                    case 2:
+                        ViewFile(Person);
+                        break;
+                    case 3:
+                        DeleteFile(Person);
+                        break;
+                    case 4:
+                        Console.WriteLine("Выходим");
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Выберите от 1 до 4");
+                        break;
+                }
+
+                Console.WriteLine("Нажмите любую клавишу");
+                Console.ReadLine();
+                Console.Clear();
+            }
         }
 
+        static void AddFile(Dictionary<string, string> Person)
+        {
+            Console.Write("Введите ФИО\t");
+            string FIO = Console.ReadLine();
+            Console.Write("Введите Должность\t");
+            string job = Console.ReadLine();
+            if (FIO != "" && job != "")
+            {
+                Person.Add(FIO, job);
+                Console.WriteLine($" {FIO} {job} добавлен");
+            }
+        }
+
+        static void ViewFile(Dictionary<string, string> Person)
+        {
+            // 1. ФИО - Должность
+            int i = 0;
+            foreach (var onePerson in Person)
+            {
+                i++;
+                Console.WriteLine($"{i}. {onePerson.Key} - {onePerson.Value}");
+            }
+        }
+
+        static void DeleteFile(Dictionary<string, string> Person)
+        {
+            ViewFile(Person);
+            Console.WriteLine("Введите номер какого досье вы желаете удалить?");
+            bool checkEnter = int.TryParse(Console.ReadLine(), out int idDelFile);
+            int i = 0;
+            foreach (var onePerson in Person)
+            {
+                i++;
+                if (i == idDelFile)
+                {
+                    Person.Remove(onePerson.Key);
+                    Console.WriteLine($"{i}. {onePerson.Key}  {onePerson.Value} - удален");
+                }
+            }
+        }
     }
 }
