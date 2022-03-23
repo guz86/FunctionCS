794f80677286a0e8724282e0e67a7f492564fcdd
Pavel Gordienko <guzmo6@rambler.ru>
2022-03-23 04:31:31 +0300

    class Player

---
+++ Junior/Task22/Player.cs
@@ -0,0 +1,18 @@
+﻿namespace Task22;
+
+public class Player
+{
+    private string _name;
+    private int _health;
+
+    public Player(string name, int health)
+    {
+        _name = name;
+        _health = health;
+    }
+
+    public void PlayerInfo()
+    {
+        Console.WriteLine($"Name: {_name}, health: {_health}");
+    }
+}
