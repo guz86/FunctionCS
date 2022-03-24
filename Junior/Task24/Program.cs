4083fc11c7cd852e693627a1455eebd21d282494
Pavel Gordienko <guzmo6@rambler.ru>
2022-03-24 03:49:30 +0300

    fix static void BanPlayer static void DelPlayer

--- Junior/Task24/Program.cs
+++ Junior/Task24/Program.cs
@@ -4,26 +4,29 @@
     {
         static void Main(string[] args)
         {
-            // База данных игроков
-            //     Реализовать базу данных игроков и методы для работы с ней.
-            //     У игрока может быть порядковый номер, ник, уровень, флаг – забанен ли он(флаг - bool).
-            //     Реализовать возможность добавления игрока, бана игрока по порядковому номеру, разбана
-            // игрока по порядковому номеру и удаление игрока.
-            //     Создание самой БД не требуется, задание выполняется инструментами,
-            // которые вы уже изучили в рамках курса.
+            /* База данных игроков
+            Реализовать базу данных игроков и методы для работы с ней.
+            У игрока может быть порядковый номер, ник, уровень, флаг – забанен ли он(флаг - bool).
+            Реализовать возможность добавления игрока, бана игрока по порядковому номеру, разбана
+            игрока по порядковому номеру и удаление игрока.
+            Создание самой БД не требуется, задание выполняется инструментами,
+            которые вы уже изучили в рамках курса.
+            */
+
             List<Player> players = new List<Player>();
             GeneratePlayer generate = new GeneratePlayer(10, players);
             while (true)
             {
-                
                 Console.WriteLine("***** База Игроков *****");
                 foreach (var player in players)
                 {
-                    Console.WriteLine($"{player.Number}.\tИмя: {player.Name}\tУровень: {player.Level}\tБан: {player.Flag}");
+                    Console.WriteLine(
+                        $"{player.Number}.\tИмя: {player.Name}\tУровень: {player.Level}\tБан: {player.Flag}");
                 }
 
                 Console.WriteLine();
-                Console.WriteLine("Выберите действие (1-5): 1 - добавить, 2 - забанить, 3 - разбанить, 4 - удалить, 5 - выход");
+                Console.WriteLine(
+                    "Выберите действие (1-5): 1 - добавить, 2 - забанить, 3 - разбанить, 4 - удалить, 5 - выход");
                 bool checkEnter = int.TryParse(Console.ReadLine(), out int enter);
                 if (checkEnter)
                 {
@@ -72,20 +75,23 @@
         {
             Console.WriteLine("Введите номер игрока:");
             int.TryParse(Console.ReadLine(), out int number);
-            foreach (var player in players)
-            {
-                if (player.Number == number) player.BanPlayer(number);
-            }
+            // foreach (var player in players)
+            // {
+            //     if (player.Number == number) player.BanPlayer(number);
+            // }
+            if(number < players.Count) players[number-1].BanPlayer(number-1);
+            
         }
 
         public static void NoBanPlayer(List<Player> players)
         {
             Console.WriteLine("Введите номер игрока:");
             int.TryParse(Console.ReadLine(), out int number);
-            foreach (var player in players)
-            {
-                if (player.Number == number) player.NoBanPlayer(number);
-            }
+            // foreach (var player in players)
+            // {
+            //     if (player.Number == number) player.NoBanPlayer(number);
+            // }
+            if(number < players.Count) players[number-1].NoBanPlayer(number-1);
         }
 
         public static void DelPlayer(List<Player> players)
