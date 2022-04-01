c3e41229e539bb8fe2584b4709711f5e94e79276
Pavel Gordienko <guzmo6@rambler.ru>
2022-04-01 14:32:06 +0300

    ver1

---
+++ Junior/Task34/Prisoner.cs
@@ -0,0 +1,13 @@
+﻿namespace Task34;
+
+public class Prisoner
+{
+    public string Fio { get; }
+    public string Сrime { get; }
+
+    public Prisoner(string fio, string сrime)
+    {
+        Fio = fio;
+        Сrime = сrime;
+    }
+}
--- Junior/Task34/Program.cs
+++ Junior/Task34/Program.cs
@@ -11,7 +11,36 @@
                 заключенных, каждый заключенный состоит из полей:
             ФИО, преступление.Вывести список до амнистии и после.
             */
-            
+
+            // список заключенных
+            List<Prisoner> prisoners = new List<Prisoner>
+            {
+                new Prisoner("Иванов", "Антиправительственное"),
+                new Prisoner("Петров", "Уголовное"),
+                new Prisoner("Сидоров", "Антиправительственное"),
+                new Prisoner("Жернов", "Административное"),
+                new Prisoner("Котов", "Антиправительственное"),
+                new Prisoner("Понкратов-Черный", "Терроризм"),
+            };
+
+            OutputPrisoners(prisoners);
+            OutputPrisoners(Amnesty(prisoners));
+        }
+
+        private static void OutputPrisoners(List<Prisoner> prisoners)
+        {
+            foreach (var prisoner in prisoners)
+            {
+                Console.WriteLine($"{prisoner.Fio} - {prisoner.Сrime}");
+            }
+
+            Console.WriteLine();
+        }
+
+        private static List<Prisoner> Amnesty(List<Prisoner> prisoners)
+        {
+            var amnestyPrisoners = prisoners.Where(prisoner => prisoner.Сrime != "Антиправительственное").ToList();
+            return amnestyPrisoners;
         }
     }
 }
