a7f791442fa96e8bfd6a8baa9dad4167fc6746f6
Pavel Gordienko <guzmo6@rambler.ru>
2022-04-01 16:28:02 +0300

    ver1

---
+++ Junior/Task36/Gamer.cs
@@ -0,0 +1,15 @@
+﻿namespace Task36;
+
+public class Gamer
+{
+    public string Name { get; }
+    public int Level { get; }
+    public int Power { get; }
+
+    public Gamer(string name, int level, int power)
+    {
+        Name = name;
+        Level = level;
+        Power = power;
+    }
+}
--- Junior/Task36/Program.cs
+++ Junior/Task36/Program.cs
@@ -12,7 +12,50 @@
                 силе, после чего вывести каждый топ.
                 2 запроса получится.
             */
+            List<Gamer> gamers = new List<Gamer>()
+            {
+                new Gamer("IvanDrago", 18, 100),
+                new Gamer("Senya", 22, 160),
+                new Gamer("Sasha", 10, 80),
+                new Gamer("Krendel", 78, 100),
+                new Gamer("Alex", 55, 200),
+                new Gamer("Ogo98", 62, 101)
+            };
+
+            ColorTitleString("Top Gamers from Server:", ConsoleColor.DarkYellow);
+            ShowTop(Top3Level(gamers), "Топ3 по уровню");
+            ShowTop(Top3Power(gamers), "Топ3 по силе");
             
+        }
+
+        public static List<Gamer> Top3Level(List<Gamer> gamers)
+        {
+            return gamers.OrderByDescending(gamer => gamer.Level).Take(3).ToList();
+        }
+
+        public static List<Gamer> Top3Power(List<Gamer> gamers)
+        {
+            return gamers.OrderByDescending(gamer => gamer.Power).Take(3).ToList();
+            
+        }
+        
+        public static void ShowTop(List<Gamer> gamers, string header)
+        {
+            ColorTitleString(header, ConsoleColor.Green);
+
+            foreach (var gamer in gamers)
+            {
+                Console.Write($"{gamer.Name}\tLevel: {gamer.Level}\tPower: {gamer.Power}");
+                Console.WriteLine();
+            }
+        }
+
+        public static void ColorTitleString(string title, ConsoleColor color )
+        {
+            ConsoleColor def = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.WriteLine(title);
+            Console.ForegroundColor = def;
         }
     }
 }
