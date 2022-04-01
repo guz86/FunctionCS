4d33b18524d1eea7e8d8042d6575b07aa2ede45d
Pavel Gordienko <guzmo6@rambler.ru>
2022-04-01 15:24:54 +0300

    ver1

--- Junior/Task35/Program.cs
+++ Junior/Task35/Program.cs
@@ -14,7 +14,73 @@
                     3)Вывести больных с определенным заболеванием
                 (название заболевания вводится пользователем с клавиатуры)
             */
+            int input = 0;
+            List<Sick> sicks = new List<Sick>()
+            {
+                new Sick("Иванов", 22, "Ветрянка"),
+                new Sick("Петров", 33, "Оспа"),
+                new Sick("Сидоров", 44, "Простуда"),
+                new Sick("Огнев", 42, "Онкология"),
+                new Sick("Смирнов", 25, "Волчанка"),
+                new Sick("Александров", 19, "Гайморит"),
+            };
             
+            while (true)
+            {
+                Console.WriteLine("1)Отсортировать всех больных по фио " + "2)Отсортировать всех больных по возрасту" +
+                                  "3)Вывести больных с определенным заболеванием" +
+                                  "4) Вывести больных");
+                int.TryParse(Console.ReadLine(), out input);
+                if (input != 0)
+                {
+                    switch (input)
+                    {
+                        case 1:
+                            SortByFioSick(sicks);
+                            break;
+                        case 2:
+                            SortByOldSick(sicks);
+                            break;
+                        case 3:
+                            ShowSpecificDiseaseSick(sicks);
+                            break;
+                        case 4:
+                            ShowSick(sicks);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+
+        private static void SortByFioSick(List<Sick> sicks)
+        {
+            sicks = sicks.OrderBy(sick => sick.Fio).ToList();
+            ShowSick(sicks);
+        }
+
+        private static void SortByOldSick(List<Sick> sicks)
+        {
+            sicks = sicks.OrderBy(sick => sick.Old).ToList();
+            ShowSick(sicks);
+        }
+
+        private static void ShowSpecificDiseaseSick(List<Sick> sicks)
+        {
+            string inputDisease = Console.ReadLine();
+            var specificDisease = sicks.Where(sick => sick.Disease == inputDisease).ToList();
+            ShowSick(specificDisease);
+        }
+        private static void ShowSick(List<Sick> sicks)
+        {
+            foreach (var sick in sicks)
+            {
+                Console.WriteLine($" > {sick.Fio}, {sick.Old}, {sick.Disease}");
+            }
         }
     }
 }
---
+++ Junior/Task35/Sick.cs
@@ -0,0 +1,15 @@
+﻿namespace Task35;
+
+public class Sick
+{
+    public string Fio { get;  }
+    public int Old { get;  }
+    public string Disease { get;  }
+    
+    public Sick(string fio, int old, string disease)
+    {
+        Fio = fio;
+        Old = old;
+        Disease = disease;
+    }
+}
