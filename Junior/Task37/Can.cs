009a5670f7092673104483d3d0963372951b1fbe
Pavel Gordienko <guzmo6@rambler.ru>
2022-04-02 06:58:00 +0300

    ver1

---
+++ Junior/Task37/Can.cs
@@ -0,0 +1,15 @@
+﻿namespace Task37;
+
+public class Can
+{
+    public string Name { get; }
+    public DateTime YearProduction { get; }
+    public int ExpirationDate { get; }
+    
+    public Can(string name, DateTime yearProduction, int expirationDate)
+    {
+        Name = name;
+        YearProduction = yearProduction;
+        ExpirationDate = expirationDate;
+    }
+}
--- Junior/Task37/Program.cs
+++ Junior/Task37/Program.cs
@@ -10,7 +10,27 @@
             получения всех просроченных банок тушенки.Чтобы не заморачиваться, можете думать, что считаем только
             года, без месяцев.
             */
-            
+            List<Can> cans = new List<Can>()
+            {
+                new Can("Банка Тушенки", new DateTime(2009, 1, 1), 4),
+                new Can("Банка Тушенки", new DateTime(2019, 1, 1), 5),
+                new Can("Банка Тушенки", new DateTime(2021, 1, 1), 2),
+                new Can("Банка Тушенки", new DateTime(2001, 1, 1), 3),
+                new Can("Банка Тушенки", new DateTime(2020, 1, 1), 1)
+            };
+
+
+            Console.WriteLine("Просроченная тушенка:");
+            foreach (var can in ExpirationCan(cans))
+            {
+                Console.WriteLine($"{can.Name}\t{can.YearProduction},{can.ExpirationDate}");
+            }
+        }
+
+        static public List<Can> ExpirationCan(List<Can> cans)
+        {
+            DateTime currentTime = DateTime.Now;
+            return cans.Where(can => (can.YearProduction.Year + can.ExpirationDate) < currentTime.Year).ToList();
         }
     }
 }
